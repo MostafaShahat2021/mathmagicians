7be695d66ba82249148b9393d5f3cc6f99a34d9d
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2023-04-03 16:37:04 +0200

    Add Home page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CalcContainer from './components/calculator';
 import Quote from './components/quote';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="app">
+      <Home />
       <Quote />
       <CalcContainer />
     </div>
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <>
+      <h2>Math Magicians</h2>
+      <h5>Welcome to our page!</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit, sit dicta accu
+        harum ullam voluptates ex numquam impedit aliquam rerum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit,
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit, sit dicta accu
+        harum ullam voluptates ex numquam impedit aliquam rerum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit,
+      </p>
+    </>
+  );
+}
+export default Home;
